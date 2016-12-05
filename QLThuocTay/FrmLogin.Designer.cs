5ce005cdb2c816a1991431b555d8caadb1db4cad
phinzin <phinzin2@gmail.com>
2016-12-05 23:17:38 +0700

    form Login

---
+++ QLThuocTay/FrmLogin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace QLThuocTay
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDn = new System.Windows.Forms.Button();
+            this.btnTh = new System.Windows.Forms.Button();
+            this.txtMk = new System.Windows.Forms.TextBox();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.lblTk = new System.Windows.Forms.Label();
+            this.LblMk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDn
+            // 
+            this.btnDn.Location = new System.Drawing.Point(180, 153);
+            this.btnDn.Name = "btnDn";
+            this.btnDn.Size = new System.Drawing.Size(75, 23);
+            this.btnDn.TabIndex = 0;
+            this.btnDn.Text = "Đăng nhập";
+            this.btnDn.UseVisualStyleBackColor = true;
+            this.btnDn.Click += new System.EventHandler(this.btnDn_Click);
+            // 
+            // btnTh
+            // 
+            this.btnTh.Location = new System.Drawing.Point(335, 153);
+            this.btnTh.Name = "btnTh";
+            this.btnTh.Size = new System.Drawing.Size(75, 23);
+            this.btnTh.TabIndex = 1;
+            this.btnTh.Text = "Thoát";
+            this.btnTh.UseVisualStyleBackColor = true;
+            this.btnTh.Click += new System.EventHandler(this.btnTh_Click);
+            // 
+            // txtMk
+            // 
+            this.txtMk.Location = new System.Drawing.Point(217, 89);
+            this.txtMk.Name = "txtMk";
+            this.txtMk.Size = new System.Drawing.Size(162, 20);
+            this.txtMk.TabIndex = 2;
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(219, 48);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(160, 20);
+            this.txtTK.TabIndex = 3;
+            // 
+            // lblTk
+            // 
+            this.lblTk.AutoSize = true;
+            this.lblTk.Location = new System.Drawing.Point(153, 51);
+            this.lblTk.Name = "lblTk";
+            this.lblTk.Size = new System.Drawing.Size(55, 13);
+            this.lblTk.TabIndex = 4;
+            this.lblTk.Text = "Tài khoản";
+            // 
+            // LblMk
+            // 
+            this.LblMk.AutoSize = true;
+            this.LblMk.Location = new System.Drawing.Point(153, 96);
+            this.LblMk.Name = "LblMk";
+            this.LblMk.Size = new System.Drawing.Size(52, 13);
+            this.LblMk.TabIndex = 5;
+            this.LblMk.Text = "Mật khẩu";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 223);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.txtMk);
+            this.Controls.Add(this.btnTh);
+            this.Controls.Add(this.btnDn);
+            this.Controls.Add(this.LblMk);
+            this.Controls.Add(this.lblTk);
+            this.Name = "FrmLogin";
+            this.Text = "FrmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDn;
+        private System.Windows.Forms.Button btnTh;
+        private System.Windows.Forms.TextBox txtMk;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Label lblTk;
+        private System.Windows.Forms.Label LblMk;
+    }
+}
---
+++ QLThuocTay/FrmLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThuocTay
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTh_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        QLTHUOCTAYDataContext context = new QLTHUOCTAYDataContext();
+        private void btnDn_Click(object sender, EventArgs e)
+        {
+
+            var result = (from LOGIN in context.LOGINs
+                         where LOGIN.USERNAME == txtTK.Text && LOGIN.Password == txtMk.Text
+                         select LOGIN).SingleOrDefault();
+
+            if (result == null)
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng! ");
+            else
+            {
+                if (string.Compare(result.AUTHORICATION, "admin")==1)
+                {
+                    MessageBox.Show("đăng nhập' "+result.AUTHORICATION+"'thành công\nđang lấy thông tin nhân vật!");
+                }
+                else
+                {
+                    MessageBox.Show("đăng nhập'"+ result.AUTHORICATION+"' thành công\nđang lấy thông tin nhân vật!");
+                }
+               
+                this.Hide();
+                Application.Exit();
+            }
+
+        }
+
+      
+
+    
+
+       
+       
+        
+    }
+}
